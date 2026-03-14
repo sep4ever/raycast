9c9216d3dcc27eb30bb9fd15240f4ed39abb648b
sep4ever <sepfourever@gmail.com>
2026-03-14 23:44:01 +0500

    Update World.cpp

--- RayMain/RayMain/World.cpp
+++ RayMain/RayMain/World.cpp
@@ -40,50 +40,48 @@ void World::Render(int rayCount, bool hitWall, float distance, float projPlaneDi
             {
                 hitWall = true;
             }
+        }
 
-            //int displacement = p->yAngle;//(int) (TILE_SIZE * p->wallScale / (distance * cos(p->yAngle)));
-            //float displacementAngle = displacement / p->rotYSpeed;
-            Vector2 displacement = {p->radAngle, p->yAngle};
+        Vector2 displacement = { p->radAngle, p->yAngle };
 
-            float corrected = distance * cos(angle - p->radAngle);
-            if (corrected < 0.0001f)
-                corrected = 0.0001f;
+        float corrected = distance * cos(angle - p->radAngle);
+        if (corrected < 0.0001f)
+            corrected = 0.0001f;
 
-            float wallHeight = (TILE_SIZE * p->wallScale / corrected) * projPlaneDist;
+        float wallHeight = (TILE_SIZE * p->wallScale / corrected) * projPlaneDist;
 
-            float columnWidth = screenWidth / rayCount;
-
-            
-
-            int drawStart = (int)((screenHeight / 2.0f) - (wallHeight / 2.f) + displacement.y);
-            int drawEnd = drawStart + (int)wallHeight;
-
-            if (drawStart < 0)
-                drawStart = 0;
-
-            if (drawEnd > screenHeight)
-                drawEnd = screenHeight;
-
-            int x = i * columnWidth;
+        float columnWidth = screenWidth / rayCount;
 
 
-            float shade = 1.0f - min((distance) / 100.0f, 1.0f);
-            unsigned char col = (unsigned char)(180.0f * shade) + 30;
 
-            Color wallColor = { col, col, col, 255 };
-			Color floorColor = { (unsigned char)(100.0f * shade), (unsigned char)(100.0f * shade) + 30, (unsigned char)(100.0f * shade) + 30, 255 };
-            if (drawEnd < screenHeight)
-            {
-                DrawRectangle(x, drawEnd , columnWidth, screenHeight - drawEnd, floorColor);
-            }
+        int drawStart = (int)((screenHeight / 2.0f) - (wallHeight / 2.f) + displacement.y);
+        int drawEnd = drawStart + (int)wallHeight;
 
-            DrawRectangle(
-                x,
-                drawStart,
-                columnWidth,
-                wallHeight,
-                wallColor
-            );
+        if (drawStart < 0)
+            drawStart = 0;
+
+        if (drawEnd > screenHeight)
+            drawEnd = screenHeight;
+
+        int x = i * columnWidth;
+
+
+        float shade = 1.0f - min((distance) / 100.0f, 1.0f);
+        unsigned char col = (unsigned char)(180.0f * shade) + 30;
+
+        Color wallColor = { col, col, col, 255 };
+        Color floorColor = { (unsigned char)(100.0f * shade), (unsigned char)(100.0f * shade) + 30, (unsigned char)(100.0f * shade) + 30, 255 };
+        if (drawEnd < screenHeight)
+        {
+            DrawRectangle(x, drawEnd, columnWidth, screenHeight - drawEnd, floorColor);
+        }
+
+        DrawRectangle(x, drawStart, columnWidth, drawEnd - drawStart, wallColor);
+        
+        // потолок
+        for (int y = 0; y < drawStart; y++)
+        {
+            DrawRectangle(x, y, columnWidth, 1, BLACK);
         }
     }
 }
